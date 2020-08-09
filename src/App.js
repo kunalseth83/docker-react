264881bf2fdfc4855b4feb2c55db5d60f0362700
kunalseth83 <seth.kunal@gmail.com>
2020-08-09 19:12:12 +0200

    How are you

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-         
+          How are you
         </p>
         <a
           className="App-link"
