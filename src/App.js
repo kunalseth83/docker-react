1b688dc9745becbca20973bee9f55952585a2950
kunalseth83 <seth.kunal@gmail.com>
2020-08-09 19:06:51 +0200

    i was changed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch
+          Hello
         </a>
       </header>
     </div>
