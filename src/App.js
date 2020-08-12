ed4dcbb86f3d2f8a8f83357a0c51ada4fe98ac33
kunalseth83 <seth.kunal@gmail.com>
2020-08-12 16:58:51 +0200

    changed App.js with HW

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello Nitya
+          Edit <code>src/App.js</code> and save to reload. Hello Worlds
         </p>
         <a
           className="App-link"
